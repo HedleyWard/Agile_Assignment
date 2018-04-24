2d690cb37408d217151749120260f225a5a6c556
Alex Kirwan <alexkirwan@Alexs-MacBook-Pro.local>
2018-04-24 09:48:13 +0100

    test-build

--- JSONCleanerLine.py
+++ JSONCleanerLine.py
@@ -5,9 +5,13 @@ import glob
 for filename in glob.iglob('**/*.json', recursive=True):
     print(hello)
     print(filename)
+    
     with open(filename, encoding="utf8") as json_file:
     data = str(json.load(json_file))
     for line in data.split():
         line.replace(".", " ")
     f = open(filename , "w")
     f.write(data)
+
+
+
